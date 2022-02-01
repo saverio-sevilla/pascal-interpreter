9f46bf0acd8a8671eebce7f194dc11178a846bec
saverio.sevilla <saveriosevilla@gmail.com>
2022-02-01 18:13:36 +0100

    Added separate execute function

--- Main.py
+++ Main.py
@@ -1,8 +1,4 @@
-from SemanticAnalysis import *
-from Parser import Parser
-from Lexer import Lexer
-from Interpreter import Interpreter
-from Token import _build_reserved_keywords
+from SPI import execute
 
 
 
@@ -34,21 +30,8 @@ def main():
     end.  { Main }
 
     """
-    RESERVED_KEYWORDS = _build_reserved_keywords()
-    lexer = Lexer(text)
-    parser = Parser(lexer)
-    tree = parser.parse()
 
-    semantic_analyzer = SemanticAnalyzer()
-    try:
-        semantic_analyzer.visit(tree)
-    except Exception as e:
-        print(e)
-
-    interpreter = Interpreter(tree)
-    result = interpreter.interpret()
-
-
+    execute(text)
 
 if __name__ == '__main__':
     main()
---
+++ SPI.py
@@ -0,0 +1,18 @@
+from SemanticAnalysis import *
+from Parser import Parser
+from Lexer import Lexer
+from Interpreter import Interpreter
+
+def execute(text):
+    lexer = Lexer(text)
+    parser = Parser(lexer)
+    tree = parser.parse()
+
+    semantic_analyzer = SemanticAnalyzer()
+    try:
+        semantic_analyzer.visit(tree)
+    except Exception as e:
+        print(e)
+
+    interpreter = Interpreter(tree)
+    result = interpreter.interpret()
